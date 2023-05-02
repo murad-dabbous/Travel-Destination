ee44bc74ed8753d1de8001e9ca79e6586d3551b1
murad-dabbous <moradyousef04@gmail.com>
2023-05-02 21:12:54 +0000

    Added the new requirements for lab12

--- travel-destination/src/components/home/Home.js
+++ travel-destination/src/components/home/Home.js
@@ -2,12 +2,12 @@ import { Fragment } from "react";
 import Header from "../header/Header";
 import Tours from "../tours/Tours";
 import Footer from "../footer/Footer";
-import data from "../../data/db.json";
+// import data from "../../data/db.json";
 const Home = () => {
   return (
     <Fragment>
       <Header />
-      <Tours data={data} />
+      <Tours />
       <Footer />
     </Fragment>
   );
--- travel-destination/src/components/tours/Tours.js
+++ travel-destination/src/components/tours/Tours.js
@@ -1,18 +1,6 @@
-// import data from "../../data/db.json";
-import { Fragment } from "react";
-const Tours = (props) => {
-  return (
-    <Fragment>
-      {props.data.map((dataItem) => {
-        return (
-          <div key={dataItem.id}>
-            <p>{dataItem.name}</p>
-            <img src={dataItem.image} alt={dataItem.name} />
-            <hr />
-          </div>
-        );
-      })}
-    </Fragment>
-  );
+import data from "../../data/db.json";
+import Tour from "./tour/Tour";
+const Tours = () => {
+  return <Tour data={data} />;
 };
 export default Tours;
